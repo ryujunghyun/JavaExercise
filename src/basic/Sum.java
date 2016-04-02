da37413006c2a1eb973cb6b4690cdaf69e8f3696
ryujunghyun <rjh1996@hanmail.net>
2016-04-02 22:00:29 +0900

    Update Sum.java
    for문을 이용해서 num이라는 변수에 값을 입력받고 그들의 합을 total에 저장해서 평균을 구하도록 만들었다.

--- src/basic/Sum.java
+++ src/basic/Sum.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class jh {
     public static void main(String[] args) {
-        float Sum = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        Sum = scanner.nextFloat();
-        Sum += scanner.nextFloat();
-        Sum += scanner.nextFloat();
-        Sum += scanner.nextFloat();
-        Sum += scanner.nextFloat();
-
-        System.out.println("평균:"+Sum / 5);
+        float total=0;
+        int num;
+        Scanner a=new Scanner(System.in);
+        for(int i=0; i<5; i++){
+            num=a.nextInt();
+            total+=num;
+        }
+        System.out.print("평균"+total/5);
     }
 }
