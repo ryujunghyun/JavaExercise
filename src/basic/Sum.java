2565697fc1b3c2f2172be2ffd41f41357397c86f
ryujunghyun <rjh1996@hanmail.net>
2016-04-02 21:29:20 +0900

    Update Sum.java
    float형의 수 5개를 입력 받아서 입력하는대로 합을 Sum에 저장을 한 후 5로 나누어서 평균을 구했다.

--- src/basic/Sum.java
+++ src/basic/Sum.java
@@ -1,18 +1,16 @@
-package basic;
-
 import java.util.Scanner;
 
-public class Sum {
+public class jh {
     public static void main(String[] args) {
-        float fSum = 0;
+        float Sum = 0;
 
         Scanner scanner = new Scanner(System.in);
-        fSum = scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
+        Sum = scanner.nextFloat();
+        Sum += scanner.nextFloat();
+        Sum += scanner.nextFloat();
+        Sum += scanner.nextFloat();
+        Sum += scanner.nextFloat();
 
-        System.out.println(fSum / 5);
+        System.out.println("평균:"+Sum / 5);
     }
 }
