294597880a29b789465f9dac6b79fc9296f3a2f0
ryujunghyun <rjh1996@hanmail.net>
2016-06-12 17:10:48 +0900

    Update Lake.java
    틀린게있어서 다시 올립니다

--- src/classes/Lake.java
+++ src/classes/Lake.java
@@ -1,31 +1,36 @@
-
-/*1. Lake의 Fish를 10마리로 늘려보자.
-Fish를 배열로 만들 것!
-
-2. 10마리 Fish 중 홀수 인덱스는 원래 대로 랜덤하게 움직이게 하고, 짝수 인덱스 Fish는 x, y를 무조건 1씩 증가하도록 움직이게 해보자.
-새로운 Fish2 클래스를 만들어도 되고, Fish 클래스에 새로운 움직임 메소드를 만들어도 됨.*/
 import java.util.Scanner;
 
-/**
- * Created by jyheo on 2016-04-03.
- */
 class Fish {
     private String name;
     private String shape;
-    private int x, y;
+    protected int x, y;
 
     public Fish(String name, String shape) {
         this.name = name;
-        //this.shape = shape;
-       // x = 0;
+        this.shape = shape;
+        x = 0;
         y = 0;
     }
+
     public Fish() {
         this.name = "Unknown";
         this.shape = "<--<";
         x = 0;
         y = 0;
     }
+
+    public void move(int width, int height) {
+        double rand = Math.random();
+        if (rand < 0.5)
+            x++;
+        else
+            y++;
+        if (x >= width)
+            x = 0;
+        if (y >= height)
+            y = 0;
+    }
+
     public void move2(int width, int height){
         x++;
         y++;
@@ -34,58 +39,56 @@ class Fish {
         if(y>=height)
             y=0;
     }
-
-    public void display(int x, int y){
-     if(this.x==x&&this.y==y){
-         System.out.print(shape);
-
-    }
-
-    
-
-
+    public void display(int x, int y) {
+        if (this.x == x && this.y == y) {
+            System.out.print(shape);
+        }
     }
 }
 
 public class Lake {
     private int width;
     private int height;
-    private Fish[] fish= new Fish[10];//배열 10개
+    private Fish []fish;
 
     public Lake(int width, int height) {
         this.width = width;
         this.height = height;
-        for(int i=0; i<fish.length; i++){
-            fish[i]=new Fish("fish"+(i), "<--<");
+        for(int i=0; i<10; i++){
+            fish[i]=new Fish();
         }
+       // fish = new Fish();
+        //fish2 = new Fish("p", "<***<");
     }
 
     public void moveFish() {
-        for(int i=0; i<10; i++){
-            switch (i%2){
-                case 0:
-                    fish[i].move2(width, height);
-                    break;
-                case 1:
-                    fish[i].move(width, height);
-                    break;
-            }
-        }
+        for(int k=0; k<10; k++){
+            if(k%2==1)
+                fish[k].move(width, height);
+             else
+                fish[k].move2(width, height);
+        }//물고기 움직임임
+
     }
 
     public void display() {
-        for (int i = 0; i < height; i++)
-            System.out.print("|");
-        for(int j=0; j<width; j++){
-            for(int k=0; k<10; k++){
-                fish[k].display(j, i);
-            }
-            System.out.print(" ");
-        }
-        System.out.println("|");
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width; i++)
             System.out.print("-");
-            System.out.println();
+        System.out.println();
+        for (int i = 0; i < height; i++) {
+            System.out.print("|");
+            for (int j = 0; j < width; j++) {
+                for(int k=0; k<10; k++){
+                    fish[k].display(j, i);
+                }
+                System.out.print(" ");
+
+            }
+            System.out.println("|");
+        }
+        for (int i = 0; i < width; i++)
+            System.out.print("-");
+        System.out.println();
     }
 
     public static void main(String args[]) {
